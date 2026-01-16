926e1b5ecebdfd8808ae020f4e3ea8ca001cee28
louisthecat <louiskatze1@web.de>
2026-01-16 06:15:57 +0000

    feat: add apk build

---
+++ android/app/src/main/java/com/braadart/checkouttrainer/MainActivity.java
@@ -0,0 +1,5 @@
+package com.braadart.checkouttrainer;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.braadart.checkouttrainer',
+  appName: 'Dart Checkout Trainer',
+  webDir: 'dist'
+};
+
+export default config;
